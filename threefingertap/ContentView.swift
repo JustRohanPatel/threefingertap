467d658b949c3f717d7063f0b298cf74ac608b16
Rohan Patel <rpatel5@bsd@220.org>
2024-11-12 10:05:55 -0600

    box done

--- threefingertap/ContentView.swift
+++ threefingertap/ContentView.swift
@@ -8,16 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var letter = ""
+    @State private var counter = 0
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Three Letter Word")
+                .font(.title.bold())
+                .padding()
+            Text("Tap the Gray box to chnage letter")
         }
-        .padding()
     }
 }
+
+struct customLetterBox: View {
+    let color : Color
+    let text : String
+    var body: some View {
+        ZStack {
+            color
+            Text(text)
+                .font(.system(size: 90))
+                .fontWeight(.heavy)
+        }
+        .frame(width: 120, height: 120)
+    }
+}
+
+
+
 
 #Preview {
     ContentView()
